d869f960a59b42ab71bebfcb02e84509e11befb6
KikiopeShodipe <gilliansifuma@gmail.com>
2025-03-10 18:50:28 +0100

    Getting started with programming

---
+++ main.py
@@ -0,0 +1,15 @@
+#exploring the print statement
+#printing a world
+print("Hello")
+#printing a sentence
+print("Hello world, I am getting started with programing")
+#printing a number
+print(12)
+#printing a number
+print(12.5)
+#printing a sentence then a blank line
+print("Hello world \n")
+#print a sentence together with a number
+print("Hello world, I am getting started with Programing",12)
+#add a * at the end of a sentence
+print("Hello world,", end="*")
---
+++ maine2.py
@@ -0,0 +1,7 @@
+#create a variable
+name="Okarun"
+Grade=10
+school="unknown"
+country="unknown"
+#print the variable of the variable in one sentence
+print("My name is ",name,"and I am in grade ",Grade,". I go to ",school," school and I came from ",country)
